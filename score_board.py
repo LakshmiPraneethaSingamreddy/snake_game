387ac85e0566b30c200368d3fb600a022a84080f
Praneetha Singamreddy <praneethasingamreddy3009@gmail.com>
2025-07-10 16:49:51 -0600

    Added High score

--- score_board.py
+++ score_board.py
@@ -8,18 +8,26 @@ class Score(Turtle):
         self.color('white')
         self.goto(0,260)
         self.score=0
+        self.high_score=0
         self.write_score()
 
     def write_score(self,):
-        self.write(f"Score: {self.score}",align='center',font=('Arial',20,'normal'))
+        self.clear()
+        self.write(f"Score: {self.score},High Score: {self.high_score}",align='center',font=('Arial',20,'normal'))
 
     def increase_score(self):
-        self.clear()
         self.score+=1
         self.write_score()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER!!", align='center', font=('Arial', 20, 'normal'))
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER!!", align='center', font=('Arial', 20, 'normal'))
+
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score=0
+        self.write_score()
+
 
 
